64eb2c4e318406fc36ab11db0452025501b44e7b
Volker Dobler <github@volker-dobler.de>
2019-11-14 22:15:12 +0100

    Fixed minor errors in evaluation expr

--- src/extension.ts
+++ src/extension.ts
@@ -123,7 +123,7 @@ function getRegexps() {
     cast: "[ifsb]",
     expr: ".+?",
     stopexpr: ".+?",
-    exprmode: "^(?<cast> {cast})?\|(~ (?<format> {format} ::))? (?<expr> {expr}) (@(?<stopexpr> {stopexpr}))?(?<reverse> !)? $",
+    exprmode: "^(?<cast> {cast})?\\|(~ (?<format> {format}) ::)? (?<expr> {expr}) (@(?<stopexpr> {stopexpr}))?(?<reverse> !)?$",
     insertnum: "^(?<start> {signednum})? (:(?<step> {signednum}))? (~(?<format> {format}))?(::(?<expr> {expr}))? (@ (?<stopexpr> {stopexpr}) )? (?<reverse> !)? $",
     insertalpha: "^(?<start> {alphastart})(: (?<step> {signedint}) )? (~ (?<format> {alphaformat})(?<wrap> w)?)?(@(?<stopexpr> {stopexpr}) )?(?<reverse> !)?$"
   };
@@ -150,6 +150,8 @@ function getRegexps() {
 
 function InsertNumsCommand() {
   let document = vscode.window;
+  
+  const maxDecimals:number = 20;
   
   if (vscode === undefined || vscode.window === undefined || vscode.window.activeTextEditor === undefined) {
     vscode.window.showErrorMessage("Extension only available with active Texteditor");
@@ -217,7 +219,11 @@ function InsertNumsCommand() {
       const alphaformat_align = (groups as any).alphaformat_align;
       const alphaformat_integer = (groups as any).alphaformat_integer;
       
-      // let decimals = ((groups as any).step && (groups as any).step.indexOf(".") > -1) ? (((groups as any).step.length - (groups as any).step.indexOf(".") - 1) <= 20 ? (groups as any).step.length - (groups as any).step.indexOf(".") - 1 : 20) : 0;
+      let decimals = ((groups as any).step && (groups as any).step.indexOf(".") > -1) ? (((groups as any).step.length - (groups as any).step.indexOf(".") - 1) <= maxDecimals ? (groups as any).step.length - (groups as any).step.indexOf(".") - 1 : maxDecimals) : 0;
+      
+      if (format.length > 0) {
+        decimals = (format.indexOf('.') > -1) ? (format.length - format.indexOf('.') - 1) : decimals;
+      }
       
       let values:any = [];
       let value:any = 1;
@@ -225,19 +231,15 @@ function InsertNumsCommand() {
       
       if (EXPRMODE) {
       } else if (! ALPHA) {
-        if ((groups as any).start !== undefined) {
-          value = Number((groups as any).start);
-        }
-        // if ((format && format.indexOf('.')) || (+step !== (+step|0))) {
-          // decimals = Math.max((format.length - format.indexOf('.') - 1), (step.toString().length - step.toString().indexOf('.') - 1),decimals);
-        // }
+        value = ((groups as any).start !== undefined) ? Number((groups as any).start) : 1;
       } else {
-        value = alphaToNum(String((groups as any).start).toLocaleLowerCase());
+        value = ((groups as any).start !== undefined) ? alphaToNum(String((groups as any).start).toLocaleLowerCase()) : 1;
         lenVal = WRAP ? (groups as any).start.toString().length : 0;
       }
       
       let evalValue:any = 0;
       let replace:any;
+      let prevValue:number = 0;
       
       let i:number = 0;
       let skip:boolean = false;
@@ -247,10 +249,10 @@ function InsertNumsCommand() {
       let timeLimit = 1000;  // max. 1 second in the while loop
       
       let castTable = {
-        i: function(value:any) { return (Number(value) === (Number(value)|0)) ? Number(value) : null; },
-        f: function(value:any) { return (Number(value) !== (Number(value)|0)) ? Number(value) : null; },
-        s: function(value:any) { return String(value); },
-        b: function(value:any) { return Boolean(value); }
+        i: function(value:any):any { return (Number(value) === (Number(value)|0)) ? Number(value) : null; },
+        f: function(value:any):any { return (Number(value) !== (Number(value)|0)) ? Number(value) : null; },
+        s: function(value:any):string { let x = String(value); return x; },
+        b: function(value:any):boolean { return Boolean(value); }
       };
       
       let WSP = new vscode.WorkspaceEdit();
@@ -265,27 +267,31 @@ function InsertNumsCommand() {
         }
         if (EXPRMODE) {
           let range = ((selections !== null) ? ((! REVERSE) ? selections[i] : selections[selections.length -1 - i]) : null) as vscode.Range;
+          let original:string = "";
           if (vscode.window.activeTextEditor !== undefined) {
-            value = vscode.window.activeTextEditor.document.getText(range);
+            original = vscode.window.activeTextEditor.document.getText(range);
           }
           try {
 // @ts-ignore
-            value = casttable[cast](value);
+            value = castTable[cast](original);
           }
           catch(e) {
 // @ts-ignore
-            vscode.window.showErrorMessage(`[${value}] could not be cast to ${casttable[cast]}`);
+            vscode.window.showErrorMessage(`[${value}] could not be cast to ${castTable[cast]}`);
             return null;
           }
         } else {
-          let range = ((selections !== null) ? ((! REVERSE) ? selections[i] : selections.pop()) : null) as vscode.Range;
-          if (! range.isEmpty && vscode.window.activeTextEditor !== undefined) {
-            value = vscode.window.activeTextEditor.document.getText(range);
+          let range = ((selections !== null) && (i < selections.length) ? ((! REVERSE) ? selections[i] : selections[selections.length-1-i]) : null) as vscode.Range;
+          if (range !== null && ! range.isEmpty && vscode.window.activeTextEditor !== undefined) {
+            let original = vscode.window.activeTextEditor.document.getText(range);
+            value = Number.isNaN(+original) ? value : +original;
           }
         }
         if (! skip) {
           if (expr || (stop_expr !== undefined)) {
-            (groups as any).step = "";
+            if (EXPRMODE) {
+              (groups as any).step = "";
+            }
           }
           if (ALPHA) {
             evalValue = numToAlpha(value, lenVal);
@@ -294,31 +300,22 @@ function InsertNumsCommand() {
             }
           } else {
             if (expr) {
-              value = value !== null ? value : "";
-              evalStr = (groups as any).expr.replace(/_/g,value).replace(/s/gi,step).replace(/n/gi,selLen).replace(/p/gi,evalValue).replace(/c/gi,evalValue).replace(/i/gi,i+1);
+              value = (value !== null) ? value : 0;
+              evalStr = (groups as any).expr.replace(/\b_\b/g,value).replace(/\bs\b/gi,step).replace(/\bn\b/gi,selLen).replace(/\bp\b/gi,prevValue).replace(/\bc\b/gi,evalValue).replace(/\bi\b/gi,i);
               try {
                 evalValue = eval(evalStr);
-                // if (parseFloat(evalValue)) {
-                  // evalValue = decimals > 0 ? evalValue.toFixed(decimals) : evalValue;
-                // }
               }
               catch(e) {
                 vscode.window.showErrorMessage(`[${(groups as any).expr}] Invalid Expression. Exception is: ` + e);
                 return null;
               }
             } else {
-              if (matchNum) {
-                evalValue = value;
-              } else if (matchAlpha) {
-                evalValue = numToAlpha(value, lenVal);
-              } else {
-                evalValue = value;
-              }
+              evalValue = value;
             }
           }
           
           if (stop_expr !== undefined) {
-            evalStr = stop_expr.replace(/_/g,value).replace(/s/gi,step).replace(/n/gi,selLen).replace(/p/gi,evalValue).replace(/c/gi,evalValue).replace(/i/gi,i+1);
+            evalStr = stop_expr.replace(/\b_\b/g,value).replace(/\bs\b/gi,step).replace(/\bn\b/gi,selLen).replace(/\bp\b/gi,prevValue).replace(/\bc\b/gi,evalValue).replace(/\bi\b/gi,i);
             try {
               if (eval(evalStr)) { break; }
             }
@@ -327,29 +324,33 @@ function InsertNumsCommand() {
               return null;
             }
           }
-          if (format) {
+          if (format !== undefined && format.length > 0) {
             let preFormat = "%";
             replace = "";
             if (! ALPHA) {
+/* 
               if (format_sign !== undefined) { preFormat += format_sign; }
               if (format_padding !== undefined) { preFormat += "'" + format_padding; }
               if (format_align !== undefined) { if (format_align === "<") { preFormat += "-"; } }
               if (format_filled !== undefined) { preFormat += format_filled; }
               if (format_integer !== undefined) { preFormat += format_integer; }
+ */
               if (format_precision !== undefined) { 
                 preFormat += "." + format_precision; 
-                // decimals = format_precision; 
+                decimals = format_precision; 
               }
+/* 
               if (format_type !== undefined) { 
                 preFormat += format_type; 
               } else { 
-                if (format_precision !== undefined) { // || decimals > 0) {
+                if ((format_precision !== undefined) || decimals > 0) {
                   preFormat += "g";
                 } else {
                   preFormat += "d"; 
                 }
               }
-              replace = d3.format(format)(evalValue);
+ */           
+              replace = d3.format(format)((decimals > 0) ? evalValue.toFixed(decimals) : evalValue);
             } else {
               let alphaFormat:object = {};
 // @ts-ignore
@@ -362,19 +363,22 @@ function InsertNumsCommand() {
             }
 
           } else {
-            replace = evalValue; // String(decimals > 0 ? evalValue.toFixed(decimals) : evalValue);
+            replace = String(decimals > 0 ? evalValue.toFixed(decimals) : evalValue);
           }
         }
         
-        values.push((! skip) ? replace : String(value));
+        values.push((! skip) ? String(replace) : String(value));
+        prevValue = (! skip) ? +replace : +value;
         
         if (! EXPRMODE) {
           value += +step;
-          // value.toFixed(decimals);
+          value.toFixed(decimals);
         }
         i += 1;
         skip = false;
       }
+      
+      if (values.length === 0) { return null; }
       
       if (EXPRMODE) {
         if (selections !== null) {
@@ -398,7 +402,7 @@ function InsertNumsCommand() {
           selections.forEach(function (element:vscode.Range, index:number) {
             if (index >= values.length) {
               text = "";
-            } else if ((index === selLen) && (values.length > selLen)) {
+            } else if ((index + 1 === selLen) && (values.length > selLen)) {
               let other = (! REVERSE) ? values.slice(index,values.length) : values.slice(0,-index-1);
               text = other.join("\n");
             } else {
